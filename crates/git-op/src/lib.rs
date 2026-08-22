360884264ac3660732a5b066017f255d8f8898b6
Joseph D. Carpinelli <joseph.carpinelli@icloud.com>
2026-08-22 10:55:58 -0400

    docs: add global documentation to lib.rs

--- crates/git-op/src/lib.rs
+++ crates/git-op/src/lib.rs
@@ -1,1 +1,7 @@
-
+//! An operation log for Git refs, configurations, and descriptions.
+//!
+//! Git's `reference-transaction` hook receives every Git ref update before and after it's written.
+//! We can use this functionality to record the full mutable repository state --- all refs --- in Git.
+//! A special ref, exempted from the operation log, is used to store the object ID (OID) associated with every local ref, on every committed `reference-transaction` hook invocation.
+//! Remote references, e.g. `refs/remotes/origin/main`, are exempted.
+//! Local symbolic refs that are specially handled by the Git CLI, e.g. `HEAD`, are also exempted.
